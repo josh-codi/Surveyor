dda0ef60af39bd7cb6fe5142301537937311be44
josh-codi <odamejoshua37@gmail.com>
2021-10-04 16:29:24 +0200

    backend: load current user from jwt payload, instead of database

--- backend/surveyor/auth/__init__.py
+++ backend/surveyor/auth/__init__.py
@@ -1,29 +1,9 @@
-from typing import Union
-
 from flask import g
 from flask.app import Flask
-from flask_jwt_extended import get_jwt_identity
 
-from auth.service import AuthService
+from auth.service import User
+
 from .views import AuthView
-from database import get_db
-
-
-def _load_logged_in_user_on_request_start():
-    """
-    Called at the start of each request, to fetch the current user into memory
-    """
-    try:
-        user_id: Union[str, None] = get_jwt_identity()
-    except Exception as error:
-        user_id = None
-
-    if user_id is None:
-        setattr(g, 'user', None)
-    else:
-        db = get_db()
-        user = AuthService().get_user_by_id(db, user_id)
-        setattr(g, 'user', user)
 
 
 def _clear_user_on_request_end(*args, **kwargs):
@@ -31,6 +11,9 @@ def _clear_user_on_request_end(*args, **kwargs):
 
 
 def init_app(app: Flask):
-    app.before_request(_load_logged_in_user_on_request_start)
     app.teardown_request(_clear_user_on_request_end)
     AuthView.register(app, route_base='/auth')
+
+
+def get_current_user() -> User:
+    return g.user
--- backend/surveyor/auth/decorators.py
+++ backend/surveyor/auth/decorators.py
@@ -1,7 +1,10 @@
 import functools
 
 from flask import abort, g
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from typing import Union
+import logging
+from .service import User
 
 
 def login_required(view):
@@ -9,9 +12,19 @@ def login_required(view):
     if there is currently no authenticated user """
 
     @functools.wraps(view)
-    @jwt_required()
+    @jwt_required()  # <-- ensures that the Authorization header contains a valid JWT
     def wrapped_view(*args, **kwargs):
-        if g.user is None:
+        # load user instance into G object
+        try:
+            # 'user_data' is the object passed as identity when creating access_token
+            user_data: Union[str, None] = get_jwt_identity()
+        except Exception as error:
+            logging.error('Failed to get JWT identity')
+            logging.error(error)
+            user_data = None
+        setattr(g, 'user', None if user_data is None else User(**user_data))
+
+        if getattr(g, 'user', None) is None:
             abort(401, 'Not authenticated. Login is required')
 
         return view(*args, **kwargs)
